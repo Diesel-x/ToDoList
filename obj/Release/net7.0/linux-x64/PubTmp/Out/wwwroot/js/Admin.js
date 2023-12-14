3d2a6b759b20c1db68b00c7f9ee04376243a03f1
Diesel <92308937+Diesel-x@users.noreply.github.com>
2023-12-14 23:03:32 +0500

    Final Final

---
+++ obj/Release/net7.0/linux-x64/PubTmp/Out/wwwroot/js/Admin.js
@@ -0,0 +1,34 @@
+﻿var deleteComment = (commentId) => {
+    fetch("/api/Admin/DeleteComment?" + new URLSearchParams({
+        commentId: commentId
+    }), { method: 'POST' })
+        .then(res => {
+        if (res.status == 200) location.reload();
+    });
+}
+
+var deletePost = (postId) => {
+    fetch("/api/Admin/DeletePost?" + new URLSearchParams({
+        postId: postId
+    }), { method: 'POST' })
+        .then(res => {
+            if (res.status == 200) location.reload();
+        });
+}
+
+
+$("[data-btndelcomment]").each(function () {
+    $(this).click(() => {
+        var comment = $(this).data("btndelcomment");
+        deleteComment(comment);
+    });
+});
+
+
+$("[data-btndelpost]").each(function () {
+    $(this).click(() => {
+        var post = $(this).data("btndelpost");
+        deletePost(post);
+    });
+});
+
---
+++ obj/Release/net7.0/linux-x64/PubTmp/Out/wwwroot/js/Comments.js
@@ -0,0 +1,25 @@
+﻿var send_comment = (id) => {
+    fetch("/api/Comments/Create?" + new URLSearchParams({
+        relatedPostId: id,
+        Text: $(`[data-inppostid=${id}]`).val(),
+    }), { method: 'POST' }).then(res => {
+        if (res.status == 200) location.reload();
+    });
+};
+
+$("[data-btnpostid]").each(function () {
+    $(this).click(() =>
+    {
+        var id = $(this).data("btnpostid");
+        send_comment(id);
+    });
+});
+
+$("[data-inppostid]").each(function () {
+    $(this).on('keypress',(e) => {
+        if (e.which == 13) {
+            var id = $(this).data("inppostid");
+            send_comment(id);
+        }
+    })
+});
